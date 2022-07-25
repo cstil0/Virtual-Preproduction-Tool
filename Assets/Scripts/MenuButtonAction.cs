189649c84e5cca13a65504a8fd3418f5217a56c6
c2207 <102225573+cstil0@users.noreply.github.com>
2022-07-25 23:37:41 +0200

    Menu fixed finally

--- Assets/Scripts/MenuButtonAction.cs
+++ Assets/Scripts/MenuButtonAction.cs
@@ -13,43 +13,14 @@ public class MenuButtonAction : MonoBehaviour
 
     bool triggerOn;
     bool buttonDown;
+    bool isFirstTime;
 
     private void OnTriggerEnter(Collider other)
     {
-        OVRInput.Update();
-        if (other.gameObject == handController) //&& OVRInput.Get(OVRInput.Button.One))
-        {
-            var colors = button.GetComponent<Button>().colors;
-            colors.normalColor = Color.red;
-            //colors.normalColor = new Color(149, 149, 149);
-            button.GetComponent<Button>().colors = colors;
-
-            OVRInput.Update();
-            if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger) && !triggerOn)
-            {
-                if (!buttonDown)
-                {
-                    // change menu
-                    buttonDown = true;
-                    ItemsMenu itemsMenu = canvas.GetComponent<ItemsMenu>();
-                    itemsMenu.change_MenuButton(currMenuPanel, newMenuPanel);
-
-                    // debug
-                    string debugText = currMenuPanel.name + " Out\n" + newMenuPanel.name + " In";
-                    Text textComponent = debugPanelText.GetComponent<Text>();
-                    textComponent.text = debugText;
-
-                    // change color
-                    var colors = button.GetComponent<Button>().colors;
-                    colors.normalColor = Color.white;
-                    button.GetComponent<Button>().colors = colors;
-                }
-            }
-            else
-            {
-                buttonDown = false;
-            }
-        }
+        var colors = button.GetComponent<Button>().colors;
+        colors.normalColor = Color.blue;
+        //colors.normalColor = new Color(149, 149, 149);
+        button.GetComponent<Button>().colors = colors;
 
         triggerOn = true;
 
@@ -58,29 +29,71 @@ public class MenuButtonAction : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == handController)
-        {
-            var colors = button.GetComponent<Button>().colors;
-            colors.normalColor = Color.white;
-            button.GetComponent<Button>().colors = colors;
-        }
+        var colors = button.GetComponent<Button>().colors;
+        colors.normalColor = Color.white;
+        button.GetComponent<Button>().colors = colors;
 
         triggerOn = false;
+        buttonDown = false;
+
+
     }
     // Start is called before the first frame update
     void Start()
     {
+        // IGUAL NO ÉS NECESSARI FER-HO SI JA HO FA L'ENABLE
         triggerOn = false;
         buttonDown = false;
+        isFirstTime = true;
 
         string debugText = "Debug panel working correctly";
         Text textComponent = debugPanelText.GetComponent<Text>();
         textComponent.text = debugText;
     }
+    void OnEnable()
+    {
+        triggerOn = false;
+        buttonDown = false;
+        isFirstTime = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        OVRInput.Update();
+
+        // if button is pressed, controller is touching the menu and at least one time the controller has exit the menu
+        if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger) && triggerOn)
+        {
+            // do it only once when the button was pressed
+            if (!buttonDown && !isFirstTime)
+            {
+                // change menu
+                buttonDown = true;
+                ItemsMenu itemsMenu = canvas.GetComponent<ItemsMenu>();
+                itemsMenu.change_MenuButton(currMenuPanel, newMenuPanel);
+
+                //// debug
+                //string debugText = currMenuPanel.name + " Out\n" + newMenuPanel.name + " In";
+                //Text textComponent = debugPanelText.GetComponent<Text>();
+                //textComponent.text = debugText;
+
+                // change color
+                var colors = button.GetComponent<Button>().colors;
+                colors.normalColor = Color.white;
+                button.GetComponent<Button>().colors = colors;
+            }
+        }
+        else
+        {
+            buttonDown = false;
+            // used to know if it is the first time that controller exits the menu, to avoid having the controller on the button after changing menu
+            isFirstTime = false;
+        }
+
+        string debugText = isFirstTime.ToString();
+        Text textComponent = debugPanelText.GetComponent<Text>();
+        textComponent.text = debugText;
 
     }
 }
--- Assets/Scripts/ObjectButtonAction.cs
+++ Assets/Scripts/ObjectButtonAction.cs
@@ -6,68 +6,82 @@ public class ObjectButtonAction : MonoBehaviour
 {
     public GameObject button;
     public GameObject handController;
-    public GameObject panelText;
+    public GameObject debugPanelText;
     public GameObject canvas;
     public GameObject itemPrefab;
 
+
     bool triggerOn;
     bool buttonDown;
+    bool isFirstTime;
 
     private void OnTriggerEnter(Collider other)
     {
-        OVRInput.Update();
-        if (other.gameObject == handController) //&& OVRInput.Get(OVRInput.Button.One))
-        {
-            var colors = button.GetComponent<Button>().colors;
-            //colors.normalColor = Color.red;
-            colors.normalColor = new Color(149, 149, 149);
-            button.GetComponent<Button>().colors = colors;
-
-            if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger) && !triggerOn)
-            {
-                if (!buttonDown)
-                {
-                    buttonDown = true;
-                    ItemsMenu itemsMenu = canvas.GetComponent<ItemsMenu>();
-                    itemsMenu.ObjectButton(itemPrefab);
-                }
-            }
-            else
-            {
-                buttonDown = false;
-            }
-        }
+        var colors = button.GetComponent<Button>().colors;
+        colors.normalColor = Color.blue;
+        //colors.normalColor = new Color(149, 149, 149);
+        button.GetComponent<Button>().colors = colors;
 
         triggerOn = true;
+
 
         //panelText.GetComponent<Text>() = debugText;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == handController)
-        {
-            var colors = button.GetComponent<Button>().colors;
-            colors.normalColor = Color.white;
-            button.GetComponent<Button>().colors = colors;
-        }
+        var colors = button.GetComponent<Button>().colors;
+        colors.normalColor = Color.white;
+        button.GetComponent<Button>().colors = colors;
 
         triggerOn = false;
+        buttonDown = false;
     }
     // Start is called before the first frame update
     void Start()
     {
+        // POTSER NO ÉS NECESSARI FER-HO SI JA HO ESTÀ FENT EL ENABLE
         triggerOn = false;
         buttonDown = false;
+        isFirstTime = true;
+
 
         string debugText = "Debug panel working correctly";
-        Text textComponent = panelText.GetComponent<Text>();
+        Text textComponent = debugPanelText.GetComponent<Text>();
         textComponent.text = debugText;
+    }
+
+    private void OnEnable()
+    {
+        triggerOn = false;
+        buttonDown = false;
+        isFirstTime = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        OVRInput.Update();
+        // if button is pressed and hand is touching the menu, instantiate the object
+        if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger) && triggerOn)
+        {
+            // do it only once when the button is pressed
+            if (!buttonDown && !isFirstTime)
+            {
+                buttonDown = true;
+                ItemsMenu itemsMenu = canvas.GetComponent<ItemsMenu>();
+                itemsMenu.ObjectButton(itemPrefab);
+            }
+        }
+        else
+        {
+            buttonDown = false;
+            isFirstTime = false;
+        }
+
+        string debugText = isFirstTime.ToString();
+        Text textComponent = debugPanelText.GetComponent<Text>();
+        textComponent.text = debugText;
 
     }
 }
