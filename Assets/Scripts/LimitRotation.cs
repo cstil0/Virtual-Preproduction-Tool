ba031b3f0460eafb6498feec859acd101126b8a4
cstil <102225573+cstil0@users.noreply.github.com>
2022-09-26 20:54:16 +0200

    trying to fix limit positions

--- Assets/Scripts/LimitRotation.cs
+++ Assets/Scripts/LimitRotation.cs
@@ -1,3 +1,4 @@
+using Microsoft.MixedReality.Toolkit.Examples.Demos.EyeTracking;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,18 +6,24 @@ using UnityEngine;
 
 public class LimitRotation : MonoBehaviour
 {
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    bool alreadyTriggered = other.GetComponent<CheckControllerTriggered>().alreadyTriggered;
-    //    if (gameObject.transform.childCount > 0 && !alreadyTriggered)
-    //        checkChildMaterials(gameObject, true);
-    //}
+    Vector3 startPosDiff;
+    GameObject currentHand;
+    bool triggerOn;
+    bool buttonDown;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            currentHand = other.gameObject;
+            triggerOn = true;
+        }
+    }
 
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (gameObject.transform.childCount > 0)
-    //        checkChildMaterials(gameObject, false);
-    //}
+    private void OnTriggerExit(Collider other)
+    {
+        triggerOn = false;
+        buttonDown = false;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -27,16 +34,35 @@ public class LimitRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 position = new Vector3();
+        if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger) && triggerOn)
+        {
+            if (!buttonDown)
+            {
+                Vector3 handStartPos = currentHand.transform.position;
+                startPosDiff = gameObject.transform.position - handStartPos;
+                buttonDown = true;
+            }
+
+            position = currentHand.transform.position;
+        }
+        else
+        {
+            triggerOn = false;
+            buttonDown = false;
+            position = gameObject.transform.position;
+        }
         Transform attachPoint = gameObject.transform.GetChild(0);
+
+        // make it touch always the floor
+        position = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(position.x, -attachPoint.localPosition.y, position.z);
+
         RotationScale rotScale = gameObject.GetComponent<RotationScale>();
         Vector3 rotation = rotScale.rotation;
         Vector3 currRot = gameObject.transform.rotation.eulerAngles;
         Vector3 limitRot = new Vector3(rotation.x, currRot.y, rotation.y);
-        gameObject.transform.rotation = Quaternion.Euler(limitRot);
-
-        // make it touch always the floor
-        Vector3 position = gameObject.transform.position;
-        gameObject.transform.position = new Vector3(position.x, -attachPoint.localPosition.y, position.z);
+        //gameObject.transform.rotation = Quaternion.Euler(limitRot);
 
     }
 }
