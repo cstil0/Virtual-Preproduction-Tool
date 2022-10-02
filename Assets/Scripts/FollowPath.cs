aeb080e6419fda88f3ee8358347186579eb4aca0
cstil <102225573+cstil0@users.noreply.github.com>
2022-10-02 19:09:59 +0200

    implementing an object following a path

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public Vector3[] pathPositions;
+    public float posSpeed = 5.0f;
+    public float rotSpeed = 10.0f;
+    int pointsCount;
+    Vector3 startPosition;
+    Quaternion startRotation;
+
+    bool isPlaying;
+
+    void move(Vector3 targetPoint)
+    {
+        Vector3 currentPos = gameObject.transform.position;
+        Vector3 targetDirection = targetPoint - currentPos;
+
+        float posStep = posSpeed * Time.deltaTime;
+        float rotStep = rotSpeed * Time.deltaTime;
+        gameObject.transform.position = Vector3.MoveTowards(currentPos, targetPoint, posStep);
+        // compute the new formard direction where we will rotate to
+        Vector3 newForward = Vector3.RotateTowards(transform.forward, targetDirection, rotStep, 0.0f);
+        // compute the new rotation using this forward
+        gameObject.transform.rotation = Quaternion.LookRotation(newForward);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsCount = 0;
+        isPlaying = false;
+
+        startPosition = gameObject.transform.position;
+        startRotation = gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            isPlaying = !isPlaying;
+        else if (Input.GetKeyDown(KeyCode.S)){
+            isPlaying = false;
+            gameObject.transform.position = startPosition;
+            gameObject.transform.rotation = startRotation;
+            pointsCount = 0;
+        }
+
+        if (isPlaying && pointsCount < pathPositions.Length)
+        {
+            Vector3 currTarget = pathPositions[pointsCount];
+            move(currTarget);
+            if (gameObject.transform.position == currTarget)
+            {
+                pointsCount++;
+            }
+        }
+    }
+}
--- Assets/Scripts/VRCameraVisualization.cs
+++ Assets/Scripts/VRCameraVisualization.cs
@@ -10,40 +10,38 @@ public class VRCameraVisualization : MonoBehaviour
     public RenderTexture[] cameraTextures;
     // used to know which camera texture has to be shown
     int cameraCount;
+    GameObject cameraPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject cameraPanel = gameObject.transform.GetChild(0).gameObject;
+        cameraPanel = gameObject.transform.GetChild(0).gameObject;
         keyDown = false;
         cameraPanel.SetActive(false);
-        cameraCount = -1;
+        cameraCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject cameraPanel = gameObject.transform.GetChild(0).gameObject;
         OVRInput.Update();
         if (OVRInput.Get(OVRInput.Button.One))
         {
             // do it only if it is the first time it is pressed
             if (!keyDown)
             {
-                if (cameraCount == -1)
+                Material cameraMat = cameraPanel.transform.GetComponent<UnityEngine.UI.Image>().material;
+                // take the absolute value so that when cameraCount = -1 we do not get an error
+                cameraMat.mainTexture = cameraTextures[Mathf.Abs(cameraCount)];
+
+                if (cameraCount <= 0)
                     cameraPanel.SetActive(!cameraPanel.activeSelf);
-                else
-                {
-                    Material cameraMat = cameraPanel.GetComponent<Renderer>().material;
-                    cameraMat.mainTexture = cameraTextures[cameraCount];
-                }
+
                 keyDown = true;
                 cameraCount += 1;
-                cameraCount = cameraCount >= cameraTextures.Length ? cameraCount : -1;
+                // if we passed the limit of cameras return to -1 so that we hide the panel and then show it again when pressing the button
+                cameraCount = cameraCount >= cameraTextures.Length ? -1 : cameraCount;
             }
-            //if (OVRInput.GetDown(OVRInput.Button.One)) { 
-            //if (Input.GetKeyDown(KeyCode.M)) || Input.GetJoystickNames(Button.Three))
-            //{
         }
         // once it is up again, set it to false
         else
